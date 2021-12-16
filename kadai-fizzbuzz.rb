1afe62bc322bd9684d1b13524b82bcf27f66baa0
taro.kanda <ko0814chata@gmail.com>
2021-12-16 12:00:20 +0000

    12162100kadai

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,15 +1,15 @@
 def fizzbuzz(num)
   if num%3==0 and num%5==0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif num%3==0
-    puts "Fizz"
+    return "Fizz"
   elsif num%5==0
-    puts "Buzz"
+    return "Buzz"
   else
-    puts num
+    return num
   end
 end
 num_max = 100
 (1..num_max).each do |nm|
-  fizzbuzz(nm)
+  puts fizzbuzz(nm)
 end
